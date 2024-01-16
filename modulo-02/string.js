851821980677d43bf979662ac959302f3b44a29d
Everton Walker <everton.walker@mavtek.com>
2024-01-16 14:59:17 -0300

    feat: type string

---
+++ modulo-02/string.js
@@ -0,0 +1,8 @@
+"use strict";
+// String é um objeto que representa uma sequência de caracteres, seja ela com aspas simples ou duplas.
+let nome1 = 'Everton';
+let empresa = 'Walker company';
+console.log(nome1);
+// Você também pode usar os Back-Ticks
+let detalhePessoa = `${nome1} trabalha na empresa ${empresa}`;
+console.log(detalhePessoa);
---
+++ modulo-02/string.ts
@@ -0,0 +1,9 @@
+// String é um objeto que representa uma sequência de caracteres, seja ela com aspas simples ou duplas.
+
+let nome1: string = 'Everton';
+let empresa: string = 'Walker company'
+console.log(nome1);
+
+// Você também pode usar os Back-Ticks
+let detalhePessoa: string = `${nome1} trabalha na empresa ${empresa}`;
+console.log(detalhePessoa);
