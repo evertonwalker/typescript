e2fed70faff4ac3a6836080d94bfff9d5927d2f9
Everton Walker <everton.walker@mavtek.com>
2024-01-16 11:12:05 -0300

    feat: type boolean

---
+++ modulo-02/boolean.js
@@ -0,0 +1,15 @@
+"use strict";
+// Exemplo 01 
+// tipos primitivos e não objetos.
+let tarefaConcluida = true;
+let tarefaPendente = false;
+console.log(tarefaConcluida);
+console.log(tarefaPendente);
+// Exemplo 02 
+let concluido = false;
+if (concluido) {
+    console.log('Está concluída');
+}
+else {
+    console.log('Está pendente');
+}
---
+++ modulo-02/boolean.ts
@@ -0,0 +1,16 @@
+// Exemplo 01 
+
+// tipos primitivos e não objetos.
+let tarefaConcluida: boolean = true;
+let tarefaPendente: boolean = false;
+console.log(tarefaConcluida);
+console.log(tarefaPendente);
+
+// Exemplo 02 
+
+let concluido: boolean = false;
+if(concluido) {
+  console.log('Está concluída');
+} else {
+  console.log('Está pendente');
+}
